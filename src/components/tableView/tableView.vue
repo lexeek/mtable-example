62a00a3c3d4647a4daaafc1f50d6c93b796de8ba
Aleksei Tikhonov <lexeek@gmail.com>
2017-11-21 23:33:52 +0300

    Implemented table with ajax request

--- src/components/tableView/tableView.vue
+++ src/components/tableView/tableView.vue
@@ -3,25 +3,62 @@
     <button v-bind:disabled="isButtonDisabled" v-on:click="toggleTable">Toggle Table</button>
     <div v-if='isTableVisible'>this is material table <span>{{currentTime}} {{showTime()}}</span> <br>
 
-      <span><data-table :columns=columns :rows=rows :perPage="[100, 10, 25, 50, 500]"></data-table></span>
+      <span><data-table :currentPage=currentPage :sortable=sortable :columns=columns :rows=rows
+                        :perPage=perPage></data-table></span>
     </div>
   </div>
 </template>
 
 <script>
   import DataTable from 'vue-materialize-datatable'
+  import * as axios from 'axios'
 
   export default {
     components: {DataTable},
     name: 'tableView',
     data () {
       return {
-        columns: [                       // Array of objects
+        currentPage: 10,
+        sortable: true,
+        perPage: [5, 10, 25, 50, 500],
+        columns: [
+          {
+            label: 'Id',  // Column name
+            field: 'id',  // Field name from row
+            // Use dot for nested props
+            // Can be function with row as 1st param
+            numeric: true, // Affects sorting
+            html: false    // Escapes output if false.
+          },
           {
             label: 'Name',  // Column name
             field: 'name',  // Field name from row
                             // Use dot for nested props
                             // Can be function with row as 1st param
+            numeric: false, // Affects sorting
+            html: false    // Escapes output if false.
+          },
+          {
+            label: 'Email',  // Column name
+            field: 'email',  // Field name from row
+            // Use dot for nested props
+            // Can be function with row as 1st param
+            numeric: false, // Affects sorting
+            html: false    // Escapes output if false.
+          },
+          {
+            label: 'Username',  // Column name
+            field: 'username',  // Field name from row
+            // Use dot for nested props
+            // Can be function with row as 1st param
+            numeric: false, // Affects sorting
+            html: false    // Escapes output if false.
+          },
+          {
+            label: 'Website',  // Column name
+            field: 'website',  // Field name from row
+            // Use dot for nested props
+            // Can be function with row as 1st param
             numeric: false, // Affects sorting
             html: false    // Escapes output if false.
           }
@@ -38,24 +75,42 @@
       }
     },
 
+    created: function () {
+      this.fetchData()
+    },
     methods: {
       showTime () {
         setInterval(() => {
           this.currentTime = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
         }, 1000)
       },
+
+      fetchData () {
+        let vm = this
+        axios.get('https://jsonplaceholder.typicode.com/users')
+          .then(function (response) {
+            console.log('response', response.data)
+            vm.rows = response.data
+          })
+      },
       toggleTable () {
         this.isTableVisible = !this.isTableVisible
       }
+    },
+    mounted () {
+
     }
   }
 </script>
 
-<style scoped lang='scss'>
+<style lang='scss'>
 
-  .material-table{
+  .material-table {
     li {
-      list-style: none;
+      list-style: none !important;
+      margin-top: 0 !important;
     }
   }
+
+  /*}*/
 </style>
